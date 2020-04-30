b9e35657737a2441ebc42078c9eb370d6dc60f37
moseskamira <moses.african@gmail.com>
2020-04-30 17:44:52 +0300

    Implementing ApolloAndroid

---
+++ app/src/main/java/com/example/apolloandroid/ApolloConnector.java
@@ -0,0 +1,18 @@
+package com.example.apolloandroid;
+
+import com.apollographql.apollo.ApolloClient;
+
+import okhttp3.OkHttpClient;
+
+public class ApolloConnector {
+
+    private static final String BASE_URL = "https://graphql-api-spring-boot.herokuapp.com/graphql";
+
+    public static ApolloClient setupApollo(){
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+
+        return ApolloClient.builder().serverUrl(BASE_URL).okHttpClient(okHttpClient).build();
+    }
+
+}
